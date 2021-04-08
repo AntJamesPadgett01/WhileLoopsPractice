3e114a1e766ba2b2bdcefee480fe55b5f9f0b5e6
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-08 18:12:30 -0500

    Task Done

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WhileLoopsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int index = 1;
+            //while (index <= 5)
+            //{
+            //    Console.WriteLine(index);
+            //    index++;// have to increment or decrement or you get infinite loop
+            //}
+
+            int index1 = 6;
+            do
+            {
+                Console.WriteLine(index1);
+                index1++;
+            } while (index1 <= 5);
+
+
+
+        }
+    }
+}
